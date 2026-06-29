f4ea06751018d44a1784cde79ade88c1b6e24a53
“savorn1” <savornbon12345@gmail.com>
2026-06-29 10:00:48 +0700

    add ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  ssr: false,
   devtools: { enabled: true },
 
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@vueuse/nuxt', '@nuxt/eslint'],
